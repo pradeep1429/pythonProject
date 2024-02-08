c8e17ffea2478ed318b812041ea7870ec56a0d02
Pradeep Avadhanam <pradeep_avadhanam@epam.com>
2024-02-08 11:33:13 +0530

    added new modules

--- com/test_excel_order_placement.py
+++ com/test_excel_order_placement.py
@@ -4,7 +4,7 @@ from utility.Base import Base
 from utility.ExcelReader import excel_to_dict
 
 
-class TestOrderPlacement():
+class TestExcelOrderPlacement():
     test_data = excel_to_dict(Base.ROOT_PATH + "\\test_data.xlsx", "Sheet1")
     log = Base.getLogger()
     driver = None
@@ -13,18 +13,18 @@ class TestOrderPlacement():
         self.driver = setup_session
         print(self.test_data)
         for data in self.test_data:
-            loginPage = LoginPage(self.driver,self.log)
+            login_page = LoginPage(self.driver,self.log)
             order = OrderPlacement(self.driver,self.log)
             self.log.info(f"self driver ref is:{id(self.driver)}")
             self.log.info(f'title is: {self.driver.title}')
             self.log.info(f"current url is: {self.driver.current_url}")
-            loginPage.login(data.get("user_name"), data.get("password"))
-            if not order.add_item_to_cart():
+            login_page.login(data.get("user_name"), data.get("password"))
+            if order.add_item_to_cart() is False:
                 self.log.error(f"iteration {data} failed")
                 continue
             order.go_to_checkout()
             self.order_placement()
-            loginPage.logout()
+            login_page.logout()
 
 
 
